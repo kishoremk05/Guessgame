480b3a4625f63960f7caa3a2fc53c1b6b1c6d4ef
kishoremk05 <kishore.05mk@gmail.com>
2025-03-01 17:11:07 +0530

    Fix deployment path

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/Guessgame/", // ✅ This must match your GitHub repo name
+});
